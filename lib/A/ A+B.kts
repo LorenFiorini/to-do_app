35d63a9f253e7f61b649a914617b26559785f8b7
Lorenzo Fiorini <64514862+LorenFiorini@users.noreply.github.com>
2021-07-30 02:49:27 -0700

    initial commit

---
+++ lib/A/ A+B.kts
@@ -0,0 +1,22 @@
+fun readInt() = read().toInt()
+fun readDouble() = read().toDouble()
+fun readLong() = read().toLong()
+fun readStrings(n: Int) = List(n) { read() }
+fun readLines(n: Int) = List(n) { readLn() }
+fun readInts(n: Int) = List(n) { read().toInt() }
+fun readIntArray(n: Int) = IntArray(n) { read().toInt() }
+fun readDoubles(n: Int) = List(n) { read().toDouble() }
+fun readDoubleArray(n: Int) = DoubleArray(n) { read().toDouble() }
+fun readLongs(n: Int) = List(n) { read().toLong() }
+fun readLongArray(n: Int) = LongArray(n) { read().toLong() }
+
+fun main() {
+    val t = readInt()
+    for (i in 0 until t) {
+        var a =readInt()
+        var b =readInt()
+        print(a+b)
+    }
+
+
+}
